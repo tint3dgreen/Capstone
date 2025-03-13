225695054b6ff5fa263a1f2410ab22064720f0fb
colin <carscott@ramapo.edu>
2025-03-12 22:11:13 -0400

    Some Dashboard Work

---
+++ Presentation Notes.md
@@ -0,0 +1,47 @@
+# Nick
+## Salary Predictions
+### Data
+Adult.data<br>
+### Notes
+I dont like the hours worked per week graph. could use a regression line for better visibility
+
+I still dont like how The only measure for slary is either above 50k or below 50k
+
+Have you considered other models besides logistic regression? Perhaps SVM or decision trees?
+6/10 - meh
+
+## Future Work
+2 new datasets that expands the data to be more modern
+
+# David Lopez
+His project is about weather trends
+
+His data looks good. Lots of good preprocessing.
+
+what happened in 1979 to cause the large number of peaks?
+8/10
+# Sam
+Very practical idea
+
+I wonder if you can find more data to fill the gap from 2018-2025
+
+**Hypothesis Testing** WOW so impressive - Paired T-test? is that the right test to use there? oh yeah I think it is.
+
+Natural Disasters have a very large impact on home insurance prices
+9/10
+# Kevin
+Machine learning to predict volatility
+
+Made a neural Network to predict valatility. His models appear to be very effective.
+WOW did not expect the model to perform so poorly.
+
+Whats the point of the project if the GARCH model exists and is good?
+Very Ambitious
+9/10 - 10/10 if he fulfills his promisses
+
+# Tariq
+T-cell epitopes
+What does MHCrestriction mean physically>
+
+Generated data?
+boooooooooooring
--- src/Dash/app.py
+++ src/Dash/app.py
@@ -1,8 +1,10 @@
 # This is some example code that I found from the dash github.
 # I will follow this as a tutorial to get the base set up
 
-
 from dash import Dash, html, dcc
+import plotly.express as px
+# The library used to display the maps is plotly graph_objects
+import geopandas
 import plotly.express as px
 import pandas as pd
 
@@ -18,6 +20,34 @@ df = pd.DataFrame({
 
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
+#These are the lines responsible for instantiating the graph
+
+data = geopandas.read_file("../../data/raw/Neighborhood_Map_Atlas_Districts.geojson")
+
+fig_map = px.choropleth_mapbox(
+    data,
+    geojson=data.geometry,
+    locations=data.index,  # Column in gdf to use for coloring
+    mapbox_style="carto-positron",
+    center={"lat": data.geometry.centroid.y.mean(), "lon": data.geometry.centroid.x.mean()},
+    opacity=0.5,
+    zoom = 10,
+    labels={'value': 'Value'}
+)
+
+fig_map.update_geos(
+    resolution=50,
+    scope = 'usa',
+    showcoastlines=True, coastlinecolor="RebeccaPurple",
+    showland=True, landcolor="LightGreen",
+    showocean=True, oceancolor="LightBlue",
+    showlakes=True, lakecolor="Blue",
+    showrivers=True, rivercolor="Blue"
+)
+
+fig_map.update_layout(height=600,width=400, margin={"r":0,"t":0,"l":0,"b":0})
+
+
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
 
@@ -26,8 +56,8 @@ app.layout = html.Div(children=[
     '''),
 
     dcc.Graph(
-        id='example-graph',
-        figure=fig
+        id='example-graph2',
+        figure=fig_map
     )
 ])
 
